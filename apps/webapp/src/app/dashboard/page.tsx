9c06469c07a03c71af252b868320d56bc02ac257
Irere Emmy <irere2020@outlook.com>
2023-10-07 11:28:20 +0200

    add todo input

--- apps/webapp/src/app/dashboard/page.tsx
+++ apps/webapp/src/app/dashboard/page.tsx
@@ -1,11 +1,30 @@
+"use client";
 import ProtectedPage from "@/components/ProtectedPage";
+import React, { useState } from "react";
 
 export default function DashboardPage() {
+  const [todo, setTodo] = useState("");
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+  };
+
   return (
     <ProtectedPage>
-      <div>
-        <p>hello world</p>
-      </div>
+      <main className="flex justify-center items-center w-full h-full">
+        <form onSubmit={handleSubmit} className="flex gap-2 ">
+          <input
+            type="text"
+            value={todo}
+            onChange={(e) => setTodo(e.target.value)}
+            placeholder="enter your todo"
+            className="border border-stone-300 px-4 outline-none py-2"
+          />
+          <button type="submit" className="bg-green-300 px-3 rounded-md">
+            add
+          </button>
+        </form>
+      </main>
     </ProtectedPage>
   );
 }
